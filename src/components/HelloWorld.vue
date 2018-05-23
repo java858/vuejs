8e280caec4f326238d45b84a7f2a58c3c5459136
zhangcheng <649818135@qq.com>
2018-05-23 19:26:16 +0800

    暂存一个纯净版的父亲传数据给儿子的例子

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h1>{{ obj.age }}</h1>
+    <button @click="obj.age += 1">增加年龄</button>
+    
+    
+    <hr>
+    <son1-box :father-name="name" :father-obj="obj"></son1-box><hr>
+    <son2-box :father-name="name" :father-obj="obj"></son2-box>
+  </div>
+</template>
+
+<script>
+import son1 from "./son1"
+import son2 from "./son2"
+
+export default {
+  components:{
+    "son1Box": son1,
+    "son2Box": son2
+  },
+  data () {
+    return {
+      msg: 'i am father',
+      name: 'HelloWorld.vue',
+      obj: {
+        name:"HelloWorld.vue",
+        age: 50,
+        job: "singer"
+      }
+    }
+  },
+  computed: {
+    // growAge: function () {
+    //   this.obj.age += 1
+    // }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/son1.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}, my father is {{fatherName}}</h1>
+    <h1>{{fname}}</h1>
+    <h1>{{fobj}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props:[
+    "fatherName",
+    "fatherObj"
+  ],
+  data () {
+    return {
+      msg: 'i am son1',
+      fname: this.fatherName,
+      fobj: this.fatherObj,
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
